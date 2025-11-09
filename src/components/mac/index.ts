b0365288971456b2c02797a66ebd43740b413455
Samuraizan <gainngreen@gmail.com>
2025-11-09 20:19:20 +0530

    Remove legacy onboarding docs and Mac components

--- src/components/mac/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as MacBezel } from './MacBezel';
-export { default as MacScreen } from './MacScreen';
-export { default as MacDialog } from './MacDialog';
-export { default as MacButton } from './MacButton';
-export { default as MacInput } from './MacInput';
-export { default as MacProfileSetup } from './MacProfileSetup';
-
-
